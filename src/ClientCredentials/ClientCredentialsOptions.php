0abb6e19429bcbd02ab49c1d6b21adf88c413d32
david_smith <david_smith@sweetwater.com>
2025-02-02 13:25:50 -0500

    feat/cli Created `src/ClientCredentials/ClientCredentialsOptions.php`.

---
+++ src/ClientCredentials/ClientCredentialsOptions.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Zerotoprod\SpapiLwaCli\ClientCredentials;
+
+use Zerotoprod\DataModel\DataModel;
+
+class ClientCredentialsOptions
+{
+    use DataModel;
+
+    public const user_agent = 'user_agent';
+    public ?string $user_agent = null;
+
+    public const access_token = 'access_token';
+    public bool $access_token = false;
+    public const scope = 'scope';
+    public bool $scope = false;
+
+    public const token_type = 'token_type';
+    public bool $token_type = false;
+
+    public const expires_in = 'expires_in';
+    public bool $expires_in = false;
+}
