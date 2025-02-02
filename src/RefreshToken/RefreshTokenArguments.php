c4107f2861fac96396c815c5845c84867c0c04ad
david_smith <david_smith@sweetwater.com>
2025-02-02 13:25:50 -0500

    feat/cli Created `src/RefreshToken/RefreshTokenArguments.php`.

---
+++ src/RefreshToken/RefreshTokenArguments.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Zerotoprod\SpapiLwaCli\RefreshToken;
+
+use Zerotoprod\DataModel\DataModel;
+
+class RefreshTokenArguments
+{
+    use DataModel;
+
+    public const url = 'url';
+    public string $url;
+
+    public const refresh_token = 'refresh_token';
+    public string $refresh_token;
+
+    public const client_id = 'client_id';
+    public string $client_id;
+
+    public const client_secret = 'client_secret';
+    public string $client_secret;
+}
